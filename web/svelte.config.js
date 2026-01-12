d51a2fd398b43c72a177021cf400e0821972736e
timo <22354443+tnkuehne@users.noreply.github.com>
2026-01-12 10:18:45 +0100

    remove stupid stuff

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,20 +1,15 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-cloudflare";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		}),
-		alias: {
-			$components: './src/lib/components'
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $components: "./src/lib/components",
+    },
+  },
 };
 
 export default config;
